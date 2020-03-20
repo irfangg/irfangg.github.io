034813ea1f89d96232872eb8dffbc36cd6645ac8
irfangg <irfangg66@gmail.com>
2020-03-21 00:51:56 +0530

    Updates

---
+++ precache-manifest.b40b8c16cdf3da14a38c4646946d1719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9cc84327c3e5b54641aacb3b5b0dc0",
+    "url": "/irfangg/irfangg.github.io/index.html"
+  },
+  {
+    "revision": "7bc709cc725ad006eda7",
+    "url": "/irfangg/irfangg.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a3d64b96dde496b525ee",
+    "url": "/irfangg/irfangg.github.io/static/js/2.f934bd9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/irfangg/irfangg.github.io/static/js/2.f934bd9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc709cc725ad006eda7",
+    "url": "/irfangg/irfangg.github.io/static/js/main.d2de4850.chunk.js"
+  },
+  {
+    "revision": "7b63b4fbb32a23eb475b",
+    "url": "/irfangg/irfangg.github.io/static/js/runtime-main.52a721ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/irfangg/irfangg.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
