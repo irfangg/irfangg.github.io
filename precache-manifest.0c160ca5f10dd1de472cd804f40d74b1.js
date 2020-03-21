9ad7187efbb44ec1efa6cd1e0e583d75b804d8d4
irfangg <irfangg66@gmail.com>
2020-03-21 13:11:27 +0530

    Updates

---
+++ precache-manifest.0c160ca5f10dd1de472cd804f40d74b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0672994a8036a0da05e0568c5c6110f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae8cdabab974ced5b557",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2ac8f6da4fd1db806a5",
+    "url": "/static/js/2.135ca394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.135ca394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8cdabab974ced5b557",
+    "url": "/static/js/main.e8ceb2f6.chunk.js"
+  },
+  {
+    "revision": "d060983f58988f906e74",
+    "url": "/static/js/runtime-main.234d6ea4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
